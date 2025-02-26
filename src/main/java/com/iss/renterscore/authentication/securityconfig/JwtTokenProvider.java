35f15b5373f7b67054fd58eebb6387ced56f7e1d
aungtuntun-9823 <e1509823@u.nus.edu>
2025-02-26 17:26:52 +0800

    Used own secret keys.

--- src/main/java/com/iss/renterscore/authentication/securityconfig/JwtTokenProvider.java
+++ src/main/java/com/iss/renterscore/authentication/securityconfig/JwtTokenProvider.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
@@ -84,6 +85,8 @@ public class JwtTokenProvider {
     }
 
     public SecretKey signSecretKey() {
-        return Jwts.SIG.HS512.key().build();
+        byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
+
+        return new SecretKeySpec(keyBytes, "HmacSHA256"); // Jwts.SIG.HS256.key().build();
     }
 }
