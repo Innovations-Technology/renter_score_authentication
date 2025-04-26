48e2d331657707e9e28bc2687f8e071a9a18a404
aungtuntun-9823 <e1509823@u.nus.edu>
2025-04-26 20:03:58 +0800

    Updated property data.

--- src/main/java/com/iss/renterscore/authentication/controller/PropertyController.java
+++ src/main/java/com/iss/renterscore/authentication/controller/PropertyController.java
@@ -1,20 +1,19 @@
 package com.iss.renterscore.authentication.controller;
 
+import com.iss.renterscore.authentication.exceptions.AppException;
+import com.iss.renterscore.authentication.exceptions.DataAlreadyExistException;
+import com.iss.renterscore.authentication.exceptions.ResourceAlreadyInUseException;
 import com.iss.renterscore.authentication.exceptions.ResourceNotFoundException;
-import com.iss.renterscore.authentication.exceptions.UpdatePasswordException;
 import com.iss.renterscore.authentication.model.CustomUserDetails;
 import com.iss.renterscore.authentication.model.Property;
 import com.iss.renterscore.authentication.model.PropertyDto;
 import com.iss.renterscore.authentication.payloads.ApiResponse;
 import com.iss.renterscore.authentication.payloads.PropertyRequest;
-import com.iss.renterscore.authentication.payloads.UpdateRequest;
 import com.iss.renterscore.authentication.service.CurrentUser;
 import com.iss.renterscore.authentication.service.PropertyService;
-import com.iss.renterscore.authentication.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,16 +29,14 @@ public class PropertyController {
 
     private static final Logger logger = LoggerFactory.getLogger(PropertyController.class);
 
-    private final UserService userService;
     private final PropertyService propertyService;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
     @GetMapping(value = "/hello-auth")
     public ResponseEntity<String> helloGreeting() {
 
         return ResponseEntity.ok()
                 .body("<h1>Welcome to Renter Score Application</h1>" +
-                "<br /><br /><p><h3>This application is to enhance the Properties' solutions. </h3></p>");
+                        "<br /><br /><p><h3>This application is to enhance the Properties' solutions. </h3></p>");
     }
 
     @GetMapping("/properties")
@@ -49,7 +46,16 @@ public class PropertyController {
                 .orElseThrow(() -> new ResourceNotFoundException(" All Property ", "", "Not found!"));
 
         return ResponseEntity.ok(properties.stream().map(PropertyDto::new).toList());
+    }
 
+    @GetMapping("/details/{id}")
+    public ResponseEntity<?> getPropertyDetails(@CurrentUser CustomUserDetails currentUser, @PathVariable("id") Long propertyId) {
+        if (currentUser == null) return ResponseEntity.ok(null);
+
+        Property properties = propertyService.getPropertyDetails(propertyId)
+                .orElseThrow(() -> new ResourceNotFoundException("Property with id ", propertyId + "", "Not found!"));
+
+        return ResponseEntity.ok(new PropertyDto(properties));
     }
 
     @GetMapping("/created-properties")
@@ -65,38 +71,33 @@ public class PropertyController {
 
     @PostMapping("/create-property")
     public ResponseEntity<?> createProperty(@CurrentUser CustomUserDetails currentUser, @RequestPart("property") PropertyRequest request, @RequestPart(value = "file", required = false) List<MultipartFile> files) {
-        ApiResponse response = null;
+        ApiResponse response;
         try {
             response = propertyService.createProperty(currentUser, request, files)
-                    .orElseThrow(() -> new UpdatePasswordException("---Empty---", "No such user present"));
+                    .orElseThrow(() -> new DataAlreadyExistException("Property values :" + request.getBlockNo() + request.getUnitNo() + request.getPostalCode(), "Property already exist"));
         } catch (IOException e) {
-            throw new ResourceNotFoundException("User email", currentUser.getEmail(), "Not found!" );
+            throw new AppException("User " + currentUser.getEmail() + "Can't create property!", e );
         }
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/update-property")
-    public ResponseEntity<?> updateProperty(@CurrentUser CustomUserDetails currentUser, @RequestPart("property") UpdateRequest request, @RequestPart(value = "file", required = false) MultipartFile file) {
-        ApiResponse response = null;
+    public ResponseEntity<?> updateProperty(@CurrentUser CustomUserDetails currentUser,  @RequestPart("id") Long propertyId, @RequestPart("property") PropertyRequest request, @RequestPart(value = "file", required = false) List<MultipartFile> files) {
+        ApiResponse response;
         try {
-            response = userService.updateProfile(currentUser, request, file)
-                    .orElseThrow(() -> new UpdatePasswordException("---Empty---", "No such user present"));
+            response = propertyService.updateProperty(currentUser, propertyId, request, files)
+                    .orElseThrow(() -> new DataAlreadyExistException("Property values :" + request.getBlockNo() + request.getUnitNo() + request.getPostalCode(), "Property already exist"));
         } catch (IOException e) {
-            throw new ResourceNotFoundException("User email", currentUser.getEmail(), "Not found!" );
+            throw new AppException("User " + currentUser.getEmail() + "Can't update property!", e );
         }
         return ResponseEntity.ok(response);
     }
 
 
-    @PostMapping("/delete-property")
-    public ResponseEntity<?> deleteProperty(@CurrentUser CustomUserDetails currentUser, @RequestBody Long id) {
-        ApiResponse response = null;
-      /*  try {
-            response = userService.findByEmail()
-                    .orElseThrow(() -> new UpdatePasswordException("---Empty---", "No such user present"));
-        } catch (IOException e) {
-            throw new ResourceNotFoundException("User email", currentUser.getEmail(), "Not found!" );
-        }*/
+    @PostMapping("/delete-property/{id}")
+    public ResponseEntity<?> deleteProperty(@CurrentUser CustomUserDetails currentUser, @PathVariable("id") Long propertyId) {
+        ApiResponse response = propertyService.deleteProperty(currentUser, propertyId)
+                .orElseThrow(() -> new ResourceAlreadyInUseException("Property with id" + propertyId, "Property is being used!", ""));
         return ResponseEntity.ok(response);
     }
 
